eedb2933edffec596c41c5055f24cef19b9bc568
Veggente <veggente@gmail.com>
2013-09-23 16:02:15 -0700

    Initial Commit

---
+++ multihop-lqf/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  multihop-lqf
+//
+//  Created by Veggente on 9/23/13.
+//  Copyright (c) 2013 Xiaohan Kang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
